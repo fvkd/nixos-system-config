dd99fbe8dcc0b002e0443bb83a90eedff94910c2
fvkd <fvcker@skiff.com>
2024-04-13 01:35:38 -0400

    Initial commit of NixOS configuration

---
+++ configuration.nix
@@ -0,0 +1,16 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  imports = [
+    ./hardware-configuration.nix
+    ./modules/system.nix
+    ./modules/x11.nix
+    ./modules/services.nix
+    ./modules/users.nix
+    ./modules/packages.nix
+    ./modules/security.nix
+    ./modules/other.nix
+  ];
+}
