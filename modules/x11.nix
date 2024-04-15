fb08f712896b9dcefb7d1098fab7f7e002d62c26
fvkd <fvcker@skiff.com>
2024-04-15 12:03:27 -0400

    adjustments

--- modules/x11.nix
+++ modules/x11.nix
@@ -7,7 +7,8 @@
   services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment
-  services.displayManager.sddm.enable = true;
+  #services.displayManager.sddm.enable = true;
+  services.xserver.displayManager.lightdm.enable = true;
   # TODO: make and config at '~/.xinitrc'
   services.xserver.displayManager.startx.enable = true;
 
