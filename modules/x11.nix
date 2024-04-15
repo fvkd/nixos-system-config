d9e916a7fafaec24e31226bb8464645cdb00dc27
fvkd <fvcker@skiff.com>
2024-04-15 12:07:35 -0400

    yep

--- modules/x11.nix
+++ modules/x11.nix
@@ -7,13 +7,13 @@
   services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment
-  #services.displayManager.sddm.enable = true;
-  services.xserver.displayManager.lightdm.enable = true;
+  services.displayManager.sddm.enable = true;
+  #services.xserver.displayManager.lightdm.enable = true;
   # TODO: make and config at '~/.xinitrc'
   services.xserver.displayManager.startx.enable = true;
 
-  #services.xserver.desktopManager.plasma5.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+  #services.xserver.desktopManager.gnome.enable = true;
 
   # Enable other window managers
   # TODO: retrieve ratpoison config from Samsung usb
