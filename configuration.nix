db0f840681756a41be9c6d74c10a9d9a7be7beac
fvkd <fvcker@skiff.com>
2024-04-24 23:36:21 -0400

    #	modified:   modules/packages.nix # # Changes not staged for commit: #	modified:   configuration.nix #	modified:   modules/keyd.nix #	modified:   modules/packages.nix #	deleted:    modules/ratpoison.nix #	modified:   nixos-switch.log

--- configuration.nix
+++ configuration.nix
@@ -12,9 +12,20 @@
     ./modules/packages.nix
     ./modules/security.nix
     ./modules/other.nix
-    ./modules/keyd.nix
-    #./modules/ratpoison.nix
+    #./modules/keyd.nix
   ];
+
+  nixpkgs.config.packageOverrides = pkgs: {
+    keyd = pkgs.keyd.overrideAttrs (oldAttrs: rec {
+      version = "2.4.2";
+      src = pkgs.fetchFromGitHub {
+        owner = "rvaiya";
+        repo = "keyd";
+        rev = "v${version}";
+        sha256 = "1062jdr716sa0srbvig65d10km7dmvb35mjnd2cjq1maxwwilzl72";
+      };
+    });
+  };
 
   system.stateVersion = "23.11";
 }
