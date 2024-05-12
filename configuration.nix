0fc34a477b2dc92161166fbad2bdbf5ddddc5e98
vi|vi|vi <fvcker@skiff.com>
2024-05-12 10:58:39 -0400

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -4,7 +4,9 @@
   ...
 }: {
   imports = [
+    #./hmmodules/user.nix
     ./hardware-configuration.nix
+    ./home.nix
     ./modules/system.nix
     ./modules/x11.nix
     ./modules/services.nix
@@ -12,20 +14,15 @@
     ./modules/packages.nix
     ./modules/security.nix
     ./modules/other.nix
-    #./modules/keyd.nix
+    ./modules/keyd.nix
   ];
 
-  nixpkgs.config.packageOverrides = pkgs: {
-    keyd = pkgs.keyd.overrideAttrs (oldAttrs: rec {
-      version = "2.4.2";
-      src = pkgs.fetchFromGitHub {
-        owner = "rvaiya";
-        repo = "keyd";
-        rev = "v${version}";
-        sha256 = "1062jdr716sa0srbvig65d10km7dmvb35mjnd2cjq1maxwwilzl72";
-      };
-    });
-  };
-
   system.stateVersion = "23.11";
+
+  # Services
+  #services.snort.enable = true;
+
+  # Qt settings
+  qt.enable = true;
+  qt.platformTheme = "kde";
 }
